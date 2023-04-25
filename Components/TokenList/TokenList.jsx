0c7509a0ada85da2017cb261ec50527cebc3b493
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-25 08:13:03 +0300

    Updated TokenList Component

--- Components/TokenList/TokenList.jsx
+++ Components/TokenList/TokenList.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Image from "next/image"
 
-const TokenList = () => {
+//INTERNAL IMPORT
+import Style from "./TokenList.module.css"
+import images from "../../assets"
+
+const TokenList = ({ tokenDate, setOpenTokenBox }) => {
+  const data = [1, 2, 3, 4, 5, 6, 7]
   return (
-    <div>TokenList</div>
+    <div className={Style.TokenList}>
+      <p className={Style.TokenList_close}
+      onClick={()=> setOpenTokenBox(false)}>
+      <Image src={images.close} alt="close" width={50} height={50} />
+      </p>
+      <div className={Style.TokenList_title}>
+        <h2>Your Token List</h2>
+      </div>
+
+       {data.map((el, i) => (
+        <div className={Style.TokenList_box}>
+          <div className={Style.TokenList_box_info}>
+            <p className={Style.TokenList_box_info_symbol}>
+              HEY
+            </p>
+            <p>
+              <span>34</span>GOLD COIN
+            </p>
+          </div>
+        </div>
+       ))}
+    </div>
   )
 }
 
