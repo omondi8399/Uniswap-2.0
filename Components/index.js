c7f79aef119d1b5015ba27a2e839d7b01a231990
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-25 11:02:25 +0300

    Updated Components folder

--- Components/index.js
+++ Components/index.js
@@ -4,5 +4,6 @@ import NavBar from "./NavBar/NavBar"
 import SearchToken from "./SearchToken/SearchToken"
 import Toggle from "./Toggle/Toggle"
 import TokenList from "./TokenList/TokenList"
+import Token from "./Token/Token"
 
-export { HeroSection, Model, NavBar, SearchToken, Toggle, TokenList }
+export { HeroSection, Model, NavBar, SearchToken, Toggle, TokenList, Token }
