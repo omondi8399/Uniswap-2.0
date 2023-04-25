14f671fb7495e48c94b9c320130de7c90a5d0d6f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-25 11:00:44 +0300

    Updated Toggle Component

--- Components/Toggle/Toggle.jsx
+++ Components/Toggle/Toggle.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Toggle = () => {
+//INTERNAL IMPORT
+import Style from "./Toggle.module.css"
+const Toggle = ({ label }) => {
   return (
-    <div>Toggle</div>
+    <div className={Style.Toggle}>
+      <div className={Style.Toggle_switch_box}>
+        <input type="checkbox" className={Style.Toggle_checkbox} name={label} id={label} />
+        <label className={Style.Toggle_label} htmlFor={label}>
+          <span className={Style.Toggle_inner} />
+          <span className={Style.Toggle_switch} />
+        </label>
+      </div>
+    </div>
   )
 }
 
