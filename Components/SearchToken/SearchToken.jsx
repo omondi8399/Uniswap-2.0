5f563b0b93bf4a8633b1601a9bb47cd18d9eb6ef
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-26 09:55:44 +0300

    Updated SearchToken Component

--- Components/SearchToken/SearchToken.jsx
+++ Components/SearchToken/SearchToken.jsx
@@ -1,67 +1,111 @@
-import React, { useState } from 'react'
-import Image from "next/image"
+import React, { useState } from "react";
+import Image from "next/image";
 
-//INTERNAL IMPORT 
-import Style from "./SearchToken.module.css"
-import images from "../../assets"
+//INTERNAL IMPORT
+import Style from "./SearchToken.module.css";
+import images from "../../assets";
 
 const SearchToken = ({ openToken, tokens, tokenData }) => {
   //USESTATE
-  const [active, setActive] = useState(1)
+  const [active, setActive] = useState(1);
 
-  const coin = [
-    {
-      img: images.ether,
-      name: "ETH",
-    },
-    {
-      img: images.ether,
-      name: "ETH",
-    },
-    {
-      img: images.ether,
-      name: "ETH",
-    },
-    {
-      img: images.ether,
-      name: "ETH",
-    },
-    {
-      img: images.ether,
-      name: "ETH",
-    },
-  ]
+  let tokenList = [];
+  for (let i = 0; i < tokenData.length; i++) {
+    if (i % 2 == 1) tokenList.push(tokenData[i]);
+  }
+
+  // const coin = [
+  //   {
+  //     img: images.ether,
+  //     name: "ETH",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "DAI",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "DOG",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "FUN",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "WETH9",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "UNI",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "TIME",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "LOO",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "OOO",
+  //   },
+  //   {
+  //     img: images.ether,
+  //     name: "HEY  ",
+  //   },
+  // ];
   return (
     <div className={Style.SearchToken}>
       <div className={Style.SearchToken_box}>
         <div className={Style.SearchToken_box_heading}>
           <h4>Select a token</h4>
-          <Image src={images.close} alt="close" width={50} height={50} onClick={() => openToken(false)}/>
+          <Image
+            src={images.close}
+            alt="close"
+            width={50}
+            height={50}
+            onClick={() => openToken(false)}
+          />
         </div>
 
         <div className={Style.SearchToken_box_search}>
           <div className={Style.SearchToken_box_search_img}>
             <Image src={images.search} alt="img" width={20} height={20} />
           </div>
-          <input type='text' placeholder='Search name and past the address' />
+          <input type="text" placeholder="Search name and past the address" />
         </div>
 
         <div className={Style.SearchToken_box_tokens}>
-          {coin.map((el, i) => (
-            <span key={i + 1} className={active == i + 1 ? `${Style.active}` : ""}
-            onClick={() => (
-              setActive(i + 1), tokens({ name: el.name, image: el.img })
-            )}
+          {tokenList.map((el, i) => (
+            <span
+              key={i + 1}
+              className={active == i + 1 ? `${Style.active}` : ""}
+              onClick={() => (
+                setActive(i + 1),
+                tokens({
+                  name: el.name,
+                  image: el.img,
+                  symbol: el.symbol,
+                  tokenBalance: el.tokenBalance,
+                  tokenAddress: el,
+                })
+              )}
             >
-              <Image src={el.img || images.ether} alt="image" width={30} height={30} />
-              {el.name}
+              <Image
+                src={el.img || images.ether}
+                alt="image"
+                width={30}
+                height={30}
+              />
+              {el.symbol}
             </span>
-          ))
-
-          }
+          ))}
         </div>
       </div>
     </div>
-  )
-}
-export default SearchToken
+  );
+};
+
+export default SearchToken;
