fbc7f54bd38afb2e8b93ea05b392b48312ca7e25
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-27 15:57:43 +0300

    updated file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,49 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: {
+//     compilers: [
+//       {
+//         version: "0.7.6",
+//         settings: {
+//           evmVersion: "istanbul",
+//           optimizer: {
+//             enabled: true,
+//             runs: 1000,
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   networks: {
+//     hardhat: {
+//       forking: {
+//         url: "your",
+//       },
+//     },
+//   },
+// };
+
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 5000,
+        details: { yul: false },
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "your",
+        accounts: [`0x${"your"}`],
+      },
+    },
+  },
 };
