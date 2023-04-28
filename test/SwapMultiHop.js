510e234fa2020df3ba20440008191aad1f8e7263
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-28 22:08:00 +0300

    Updated contracts

--- test/SwapMultiHop.js
+++ test/SwapMultiHop.js
@@ -5,24 +5,24 @@ const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
-describe("SwapMultiHop", () => {
-  let swapMultiHop;
-  let accounts;
-  let weth;
-  let dai;
-  let usdc;
+// describe("SwapMultiHop", () => {
+//   let swapMultiHop;
+//   let accounts;
+//   let weth;
+//   let dai;
+//   let usdc;
 
-  before(async () => {
-    accounts = await ethers.getSigners(1);
+//   before(async () => {
+//     accounts = await ethers.getSigners(1);
 
-    const SwapMultiHop = await ethers.getContractFactory("SwapMultiHop");
-    swapMultiHop = await SwapMultiHop.deploy();
-    await swapMultiHop.deployed();
+//     const SwapMultiHop = await ethers.getContractFactory("SwapMultiHop");
+//     swapMultiHop = await SwapMultiHop.deploy();
+//     await swapMultiHop.deployed();
 
-    weth = await ethers.getContractAt("IWETH", WETH9);
-    dai = await ethers.getContractAt("IERC20", DAI);
-    usdc = await ethers.getContractAt("IERC20", USDC);
-  });
+//     weth = await ethers.getContractAt("IWETH", WETH9);
+//     dai = await ethers.getContractAt("IERC20", DAI);
+//     usdc = await ethers.getContractAt("IERC20", USDC);
+//   });
 
   it("swapExactInputMultihop", async () => {
     const amountIn = 10n ** 18n;
